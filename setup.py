0f8104c2f67823d56dcaa26080e434d0e1725aa6
Xavi Torelló <info@xaviertorello.cat>
2017-03-05 20:56:11 +0100

    Macedonia Pypy config

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+INSTALL_REQUIRES = ['']
+
+setup(
+    name='macedonia',
+    description='',
+    version='0.1',
+    url='',
+    author='Xavi Torelló, GoldraK',
+    author_email='info@xaviertorell.cat',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    license='General Public Licence 3',
+    provides=['macedonia'],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5"
+    ]
+)
